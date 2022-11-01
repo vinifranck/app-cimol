93665c76e76dd5d5672ff59aa98b009b0adbf49b
Vinicius F <vinifranckferreira@gmail.com>
2022-11-01 14:42:38 -0300

    test(APP-Cimo): primeiro commit

---
+++ src/components/home/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Aqui</h1>
+    </div>
+</template>
+
+    
+    <script>
+        import api from '../api/api';
+        import Loading from '../loading/Loading.vue';
+        export default {
+            name: 'HomePage',
+            components: { Loading},
+            mixins: [api],
+        }
+    </script>
